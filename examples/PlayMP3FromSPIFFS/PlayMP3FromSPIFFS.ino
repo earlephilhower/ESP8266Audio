6466df23f3be3159095516f567f53d9729c31f64
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-08-12 09:20:09 -0700

    Add WAV and PROGMEM example

--- examples/PlayMP3FromSPIFFS/PlayMP3FromSPIFFS.ino
+++ examples/PlayMP3FromSPIFFS/PlayMP3FromSPIFFS.ino
@@ -4,16 +4,21 @@
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2SNoDAC.h"
 
+// To run, set your ESP8266 build to 160MHz, and include a SPIFFS of 512KB or greater.
+// Use the "Tools->ESP8266 Sketch Data Upload" menu to write the MP3 to SPIFFS
+// Then upload the sketch normally.  
+
 AudioGeneratorMP3 *mp3;
 AudioFileSourceSPIFFS *file;
 AudioOutputI2SNoDAC *out;
+
 void setup()
 {
   WiFi.forceSleepBegin();
   Serial.begin(115200);
   delay(1000);
   SPIFFS.begin();
-  file = new AudioFileSourceSPIFFS("/jamonit.mp3");
+  file = new AudioFileSourceSPIFFS("/pno-cs.mp3");
   out = new AudioOutputI2SNoDAC();
   mp3 = new AudioGeneratorMP3();
   mp3->begin(file, out);
