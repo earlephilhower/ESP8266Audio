23a8ed87244942525c7042e46c467069dca0c49f
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-12-08 19:32:43 -0800

    Add HELIX MP3 decoder initial revision
    Include the RealNetworks HELIX MP3 decoder as AudioGeneratorMP3a.
    Right now runs fine, but requires a full 1152*2 sample buffer and
    seems to run slower than real time.

---
+++ src/libhelix-mp3/debug.h
@@ -0,0 +1,16 @@
+
+#if !defined(DEBUG_h)
+
+#include <core_pins.h>
+#include <HardwareSerial.h>
+
+
+#define DBG Serial2
+//#define DBG_SERIAL Serial
+#define DBG_BAUD		115200
+
+void initDBG();
+
+
+#define DEBUG_h
+#endif
