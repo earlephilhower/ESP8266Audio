e3e94010279de338fa1c1e68fe8dbc52676bbec8
Hendrik Langer <hendrik+dev@xd0.de>
2018-01-31 23:31:12 +0100

    use correct read for stream data on the ESP32 (fixes #38)

--- src/AudioFileSourceHTTPStream.cpp
+++ src/AudioFileSourceHTTPStream.cpp
@@ -116,7 +116,7 @@ retry:
   if (avail == 0) return 0;
   if (avail < len) len = avail;
 
-  int read = stream->readBytes(reinterpret_cast<uint8_t*>(data), len);
+  int read = stream->read(reinterpret_cast<uint8_t*>(data), len);
   pos += read;
   return read;
 }
