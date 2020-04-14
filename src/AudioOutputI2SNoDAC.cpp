c26eb7e7c38e54bb15046707e7d7d7078b725499
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-04-14 11:44:32 -0700

    Free BCK and WS pins for general use with NoDAC
    The BCK and WS pins aren't needed or used when using the I2SNoDAC
    option.  Restore their settings to whatever they were before, leaving
    them untouched.  Apps may configure the additional pins either before or
    after the I2SNoDac is created.

--- src/AudioOutputI2SNoDAC.cpp
+++ src/AudioOutputI2SNoDAC.cpp
@@ -32,6 +32,8 @@ AudioOutputI2SNoDAC::AudioOutputI2SNoDAC(int port) : AudioOutputI2S(port, false)
   SetOversampling(32);
   lastSamp = 0;
   cumErr = 0;
+  WRITE_PERI_REG(PERIPHS_IO_MUX_MTDO_U, orig_bck);
+  WRITE_PERI_REG(PERIPHS_IO_MUX_GPIO2_U, orig_ws);
 }
 
 AudioOutputI2SNoDAC::~AudioOutputI2SNoDAC()
