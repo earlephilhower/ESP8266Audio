ec6ac17f43bca979cfaa25ec12d3aa2b845115b7
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-08-08 15:04:43 -0700

    Add AAC example to repo

--- tests/host/aac.cpp
+++ tests/host/aac.cpp
@@ -3,13 +3,15 @@
 #include "AudioOutputSTDIO.h"
 #include "AudioGeneratorAAC.h"
 
+#define AAC "../../examples/PlayAACFromPROGMEM/homer.aac"
+
 int main(int argc, char **argv)
 {
     (void) argc;
     (void) argv;
-    AudioFileSourceSTDIO *in = new AudioFileSourceSTDIO("jamonit.aac");
+    AudioFileSourceSTDIO *in = new AudioFileSourceSTDIO(AAC);
     AudioOutputSTDIO *out = new AudioOutputSTDIO();
-    out->SetFilename("jamonit.aac.wav");
+    out->SetFilename("out.aac.wav");
     void *space = malloc(28000+60000);
     AudioGeneratorAAC *aac = new AudioGeneratorAAC(space, 28000+60000);
 
