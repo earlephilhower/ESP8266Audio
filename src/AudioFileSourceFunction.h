095b76bfc5148a763533adb314ac5e1af561fbfc
Hideaki Tai <hideaki.tai@gmail.com>
2021-04-12 09:11:16 +0900

    Add the GPL header

--- src/AudioFileSourceFunction.h
+++ src/AudioFileSourceFunction.h
@@ -1,3 +1,23 @@
+/*
+  AudioFileSourceFunction
+  Audio ouptut generator which can generate WAV file data from function
+
+  Copyright (C) 2021  Hideaki Tai
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _AUDIOFILESOURCEFUNCTION_H
 #define _AUDIOFILESOURCEFUNCTION_H
 
