e3617b1537ba10ce37838104aeee748a50e420d4
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-05-18 21:11:23 -0700

    Add LWIP to ESP8266 builds

--- tests/common.sh
+++ tests/common.sh
@@ -198,7 +198,7 @@ if [ "$BUILD_TYPE" = "build" ]; then
     install_arduino
     install_esp8266 "$HOME/arduino_ide"
     source "$HOME/arduino_ide/hardware/esp8266com/esp8266/tests/common.sh"
-    build_sketches "$HOME/arduino_ide" "$TRAVIS_BUILD_DIR" "-l $HOME/Arduino/libraries" "$BUILD_MOD" "$BUILD_REM"
+    build_sketches "$HOME/arduino_ide" "$TRAVIS_BUILD_DIR" "-l $HOME/Arduino/libraries" "$BUILD_MOD" "$BUILD_REM" "lm2f"
 elif [ "$BUILD_TYPE" = "build_esp32" ]; then
     install_arduino
     install_esp32 "$HOME/arduino_ide"
