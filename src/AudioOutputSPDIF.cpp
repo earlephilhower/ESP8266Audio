879fe48b787e5c9e06c92ab73840503ef215432f
Earle F. Philhower, III <earlephilhower@yahoo.com>
2022-05-19 13:17:07 -0700

    Clean up ESP32 I2S interface undefineds (#533)

--- src/AudioOutputSPDIF.cpp
+++ src/AudioOutputSPDIF.cpp
@@ -102,7 +102,11 @@ AudioOutputSPDIF::AudioOutputSPDIF(int dout_pin, int port, int dma_buf_count)
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // lowest interrupt priority
     .dma_buf_count = dma_buf_count,
     .dma_buf_len = DMA_BUF_SIZE_DEFAULT, // bigger buffers, reduces interrupts
-    .use_apll = true // Audio PLL is needed for low clock jitter
+    .use_apll = true, // Audio PLL is needed for low clock jitter
+    .tx_desc_auto_clear = true, // Silence on underflow
+    .fixed_mclk = 0, // Unused
+    .mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT, // Unused
+    .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT // Use bits per sample
   };
   if (i2s_driver_install((i2s_port_t)portNo, &i2s_config_spdif, 0, NULL) != ESP_OK) {
     audioLogger->println(F("ERROR: Unable to install I2S drivers"));
@@ -147,6 +151,7 @@ bool AudioOutputSPDIF::SetPinout(int bclk, int wclk, int dout)
 {
 #if defined(ESP32)
   i2s_pin_config_t pins = {
+    .mck_io_num = 0, // unused
     .bck_io_num = bclk,
     .ws_io_num = wclk,
     .data_out_num = dout,
