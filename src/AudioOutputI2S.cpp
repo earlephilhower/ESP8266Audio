f17813b95d6aadf93b9ea42f4c13da1e528da7a5
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-08-11 20:56:09 +0400

    Fix compile for esp32 platform

--- src/AudioOutputI2S.cpp
+++ src/AudioOutputI2S.cpp
@@ -88,7 +88,6 @@ bool AudioOutputI2S::SetPinout()
       return false; // Not allowed
 
     i2s_pin_config_t pins = {
-        .mck_io_num = 0, // Unused
         .bck_io_num = bclkPin,
         .ws_io_num = wclkPin,
         .data_out_num = doutPin,
@@ -229,8 +228,6 @@ bool AudioOutputI2S::begin(bool txDAC)
           .use_apll = use_apll, // Use audio PLL
           .tx_desc_auto_clear = true, // Silence on underflow
           .fixed_mclk = 0, // Unused
-          .mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT, // Unused
-          .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT // Use bits per sample
       };
       audioLogger->printf("+%d %p\n", portNo, &i2s_config_dac);
       if (i2s_driver_install((i2s_port_t)portNo, &i2s_config_dac, 0, NULL) != ESP_OK)
