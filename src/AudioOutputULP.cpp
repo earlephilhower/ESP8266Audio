d3e18fd8badf4fbd9dac0eab9169405f2c49e7f0
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2023-04-25 21:42:55 +0200

    ULP - no ESP32S2 / S3
    On ESP32S2 it compiles, but no audio is coming out
    On ESP32S3 DAC is not supported

--- src/AudioOutputULP.cpp
+++ src/AudioOutputULP.cpp
@@ -22,7 +22,7 @@
 
 #include "AudioOutputULP.h"
 
-#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#if CONFIG_IDF_TARGET_ESP32
 
 #include <esp32/ulp.h>
 #include <driver/rtc_io.h>
