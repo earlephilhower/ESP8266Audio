31cfb6c3f9f9d566a8a7459c0f4a0f3cfb76d219
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-04-16 16:21:44 -0700

    Remove OPUS encoder, not supported on embedded

--- src/libopus/opus_encoder.c
+++ src/libopus/opus_encoder.c
@@ -24,7 +24,7 @@
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
+#if 0
 //#ifdef HAVE_CONFIG_H
 #include "config.h"
 //#endif
@@ -2781,3 +2781,4 @@ void opus_encoder_destroy(OpusEncoder *st)
 {
     opus_free(st);
 }
+#endif
