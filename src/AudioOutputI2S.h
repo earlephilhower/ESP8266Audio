de2d2589f113b9ea5aa27416e0181e95102a5156
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-10-20 15:07:58 -0700

    Astyle

--- src/AudioOutputI2S.h
+++ src/AudioOutputI2S.h
@@ -62,7 +62,7 @@ public:
     virtual bool begin() override;
     virtual bool ConsumeSample(int16_t sample[2]) override;
 #ifdef ARDUINO_ARCH_RP2040
-    // Have an optimized block pass-thru for the Pico
+    // Have an optimized block pass-through for the Pico
     virtual uint16_t ConsumeSamples(int16_t *samples, uint16_t count) override;
 #endif
     virtual void flush() override;
